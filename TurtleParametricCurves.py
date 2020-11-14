e6440e85b59a6ceb140f1d182e0023355bc8aaf4
GuyInFridge <dabdemonic@gmail.com>
2020-11-14 07:55:37 -0500

    Implement rainbow color system

--- TurtleParametricCurves.py
+++ TurtleParametricCurves.py
@@ -1,6 +1,5 @@
 import turtle
 import time
-from statistics import mean
 from math import *
 
 
@@ -8,9 +7,20 @@ from math import *
 # TODO: Rainbow animation (https://clarle.github.io/yui3/yui/docs/color/rgb-slider.html)
 # TODO: Be able to input your own curve
 
+# Turtle Graphics setup
+wn = turtle.Screen()
+wn.bgcolor("white")
+wn.title("Turtle Graphics")
+wn.setup(700, 700)
+
+pen = turtle.Turtle()
+pen.shape("turtle")
+pen.width(10)
+time.sleep(1)
+
 
 class ParametricCurve:
-    PEN_SPEED = 10  # Speed sacrifices quality :(
+    STEP_SIZE = 1/10  # Step size for pen; larger step size is faster but quality :(
 
     def __init__(self, xeq, yeq, t_min, t_max, mag):
         self.t_min = t_min
@@ -28,7 +38,13 @@ class ParametricCurve:
         self.mag_x = self.mag * self.x
         self.mag_y = self.mag * self.y
 
+        self.stage = 1  # For colour changing
+
     def update_coords(self, t_input):
+        """
+        Re-evaluates the coordinates for a new t value
+        :param t_input: new t-value to re-evaluate the coordinates at
+        """
         t = t_input
 
         self.x = eval(self.xeq)  # Do I need to update this way
@@ -41,23 +57,54 @@ class ParametricCurve:
 
         pen.up()
         pen.goto(self.mag_x, self.mag_y)
+        pen.color(1, 0, 0)  # reset color to red (beginning of rainbow)
         pen.down()
 
-    @staticmethod
-    def increment_color(turtle):
+    def increment_color(self, input_turtle):
         """
-        :param turtle: turtle from Turtle
-        :param pencolor: pencolor (r,g,b) tuple as per Python turtle documentation
+        Increments colors in a rainbow fashion
+        :param input_turtle: turtle from Turtle
         :return: new pen color tuple
         """
         # TODO: Account for non-rgb colors (e.g. "blue")
-        COLOR_SPEED = 0.2  # Value from 0 to 0.2
+        COLOR_SPEED = 0.5
 
-        current_color = turtle.pencolor()
-        sign = 1 if mean(current_color) < 0.8 else -1
-        increment = tuple(sign * COLOR_SPEED for _ in range(3))
-        new_pencolor = tuple(p + q for p, q in zip(current_color, increment))  # Element-wise incrementing color
-        turtle.pencolor(new_pencolor)
+        current_color = input_turtle.pencolor()
+
+        if current_color == (1, 0, 0):
+            self.stage = 1
+        elif current_color == (1, 1, 0):
+            self.stage = 2
+        elif current_color == (0, 1, 0):
+            self.stage = 3
+        elif current_color == (0, 1, 1):
+            self.stage = 4
+        elif current_color == (0, 0, 1):
+            self.stage = 5
+        elif current_color == (1, 0, 1):
+            self.stage = 6
+
+        # increment (r,g,b) color appropriately
+        increment = (0, 0, 0)  # default
+        BASE_INCREMENT = 0.1
+
+        if self.stage == 1:
+            increment = (0, COLOR_SPEED * BASE_INCREMENT, 0)   # Add green
+        elif self.stage == 2:
+            increment = (-COLOR_SPEED * BASE_INCREMENT, 0, 0)  # Remove red
+        elif self.stage == 3:
+            increment = (0, 0, COLOR_SPEED * BASE_INCREMENT)   # Add blue
+        elif self.stage == 4:
+            increment = (0, -COLOR_SPEED * BASE_INCREMENT, 0)  # Remove green
+        elif self.stage == 5:
+            increment = (COLOR_SPEED * BASE_INCREMENT, 0, 0)   # Add red
+        elif self.stage == 6:
+            increment = (0, 0, -COLOR_SPEED * BASE_INCREMENT)  # Remove blue
+
+        new_pencolor = tuple(p + q for p, q in zip(current_color, increment))  # Increment pen-color tuple element-wise
+        new_pencolor = tuple(1 if color > 1 else color for color in new_pencolor)  # Check for extraneous color values
+        new_pencolor = tuple(0 if color < 0 else color for color in new_pencolor)
+        input_turtle.pencolor(new_pencolor)
 
     def plot_curve(self):
         self.go_to_start()
@@ -69,7 +116,7 @@ class ParametricCurve:
                 break
 
             pen.goto(self.mag_x, self.mag_y)
-            self.t += ParametricCurve.PEN_SPEED / 100
+            self.t += ParametricCurve.STEP_SIZE
             self.increment_color(pen)
 
 
@@ -77,22 +124,9 @@ class ParametricCurve:
 heart = ParametricCurve("16 * sin(t) ** 3", "13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)", -pi, pi, 18)
 lisajous_curve = ParametricCurve("4*sin(12/13*t)", "3*sin(t)", 0, 16 * pi, 75)
 butterfly = ParametricCurve("sin(t)*(e**cos(t)-2*cos(4*t)-(sin(t/12)**5))",
-                            "cos(t)*(e**cos(t)-2*cos(4*t)-(sin(t/12)**5))", 0, 2*pi, 50)
+                            "cos(t)*(e**cos(t)-2*cos(4*t)-(sin(t/12)**5))", 0, 2 * pi, 50)
 
 default_curves = [heart, lisajous_curve, butterfly]
-
-# Turtle Graphics setup
-wn = turtle.Screen()
-wn.bgcolor("white")
-wn.title("Turtle Graphics")
-wn.setup(700, 700)
-
-pen = turtle.Turtle()
-pen.color(0.1, 0.5, 0.6)
-pen.shape("turtle")
-pen.width(10)
-
-time.sleep(1)
 
 
 def main():
